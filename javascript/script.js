fdf77a8fdb0b7bf7fd248ebe3cee28a017ecd2e5
NycollSM <Nycollsotomorales21@gmail.com>
2019-02-16 20:03:11 -0600

    changin classes with BEM

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,11 @@
-const div = document.getElementById('activityGithub');
 
 function fetchJson() {
+    const div = document.getElementById('activityGithub');
     fetch('https://api.github.com/users/NycollSM/events/public')
       .then(res => res.json())
       .then(data => {
       console.log(data);
-      
-      for (const repositories of data){ 
+      for ( const repositories of data){
         const divInfo = document.createElement('div');
         divInfo.setAttribute('class', 'git_info');
         const repoName = document.createElement("p");
@@ -16,14 +15,14 @@ function fetchJson() {
         const url = document.createElement('a');
         url.setAttribute('href', repositories.repo.url);
         url.innerText = 'url';
-        console.log(repositories.repo.url);
+       // console.log(repositories.repo.url);
         const date = document.createElement('p');
         date.innerText = repositories.created_at;
         divInfo.appendChild(repoName);
         divInfo.appendChild(commit);
         divInfo.appendChild(date);
-        divInfo.appendChild(url);
         div.appendChild(divInfo);
+        divInfo.appendChild(url);
       } 
     });
 } fetchJson();

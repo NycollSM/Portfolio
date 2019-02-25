0789b47d3f7d3afd8569500394f431d69b67dc28
NycollSM <Nycollsotomorales21@gmail.com>
2019-02-24 19:40:52 -0600

    validation of form with regular expressions

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,10 @@
-
+//github activity
 function fetchJson() {
     const div = document.getElementById('activityGithub');
     fetch('https://api.github.com/users/NycollSM/events/public')
       .then(res => res.json())
       .then(data => {
-      console.log(data);
+      //console.log(data);
       for ( const repositories of data){
         const divInfo = document.createElement('div');
         divInfo.setAttribute('class', 'git_info');
@@ -15,7 +15,7 @@ function fetchJson() {
         const url = document.createElement('a');
         url.setAttribute('href', repositories.repo.url);
         url.innerText = 'url';
-       // console.log(repositories.repo.url);
+      // console.log(repositories.repo.url);
         const date = document.createElement('p');
         date.innerText = repositories.created_at;
         divInfo.appendChild(repoName);
@@ -26,3 +26,26 @@ function fetchJson() {
       } 
     });
 } fetchJson();
+
+//form validation 
+
+function FormValidation() {
+  //\s?\D?
+  const emailExpression = /"[a-z]+[@]+[a-z]"/gm;
+  const nameExpression = /"[a-z]+"/gm;
+  const test = document.getElementsByTagName('input');
+  function prueba (){
+    if (test[0] == nameExpression ){
+      alert('jxbsbsj'); //test[1] == emailExpression
+    }else{
+      alert('asdfghjkljhgfdsasdfghj');
+    }
+    
+    
+
+    
+  }
+  const button = document.getElementById('send-form');
+  
+  button.addEventListener('click', prueba);
+}FormValidation();

19fd1af2e518e5e03133cb7f2e468246a4ca5bb8
NycollSM <nycollsotomorales21@gmail.com>
2019-02-26 17:54:17 -0600

    fixing response of fetch

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,48 @@
+(function (){
 //github activity
 function fetchJson() {
     const div = document.getElementById('activityGithub');
-    fetch('https://api.github.com/users/NycollSM/events/public')
-      .then(res => res.json())
-      .then(data => {
-      //console.log(data);
-      for ( const repositories of data){
-        const divInfo = document.createElement('div');
-        divInfo.setAttribute('class', 'git_info');
-        const repoName = document.createElement("p");
-        repoName.innerText = 'Repository:' + ' ' + repositories.repo.name;
-        const commit = document.createElement('p');
-        commit.innerText = 'Commit:' + ' ' +repositories.payload.commits[0].message;
-        const url = document.createElement('a');
-        url.setAttribute('href', repositories.repo.url);
-        url.innerText = 'url';
-      // console.log(repositories.repo.url);
-        const date = document.createElement('p');
-        date.innerText = repositories.created_at;
-        divInfo.appendChild(repoName);
-        divInfo.appendChild(commit);
-        divInfo.appendChild(date);
-        div.appendChild(divInfo);
-        divInfo.appendChild(url);
-      } 
-    });
+    fetch('https://api.github.com/users/nycollsm/events/public')
+        .then (response => response.json())
+        .then(data => {
+          const items = data.slice(0,12);
+          console.log(data);
+          for (const repositories of items){
+            const divInfo = document.createElement('div');
+            divInfo.setAttribute('class', 'git--items');
+            const repoName = document.createElement('p');
+            repoName.innerText = 'Repository' + ' ' + repositories.repo.name;
+           // const commit = document.createElement('p');
+            //commit.innerText = 'Commit:' + ' ' +repositories.payload.commits[0].message;
+            const url = document.createElement('a');
+            url.setAttribute('href', repositories.repo.url);
+            url.innerText = 'url';
+            console.log(repositories.repo.url);
+            const date = document.createElement('p');
+            date.innerText = repositories.created_at;
+            divInfo.appendChild(repoName);
+             // divInfo.appendChild(commit);
+            divInfo.appendChild(date);
+            div.appendChild(divInfo);
+            divInfo.appendChild(url);
+            div.appendChild(divInfo);
+            for(commit in repositories.payload) {
+              // /console.log(commit);
+              for ( e of commit){
+                commit.innerText = e;
+                console.log(commit.commits);
+              }
+      
+            } 
+          }
+        })
+    
 } fetchJson();
 
 //form validation 
-
-function FormValidation() {
+/**
+ * 
+  function FormValidation() {
   //\s?\D?
   const emailExpression = /"[a-z]+[@]+[a-z]"/gm;
   const nameExpression = /"[a-z]+"/gm;
@@ -41,11 +54,9 @@ function FormValidation() {
       alert('asdfghjkljhgfdsasdfghj');
     }
     
-    
-
-    
   }
   const button = document.getElementById('send-form');
-  
   button.addEventListener('click', prueba);
 }FormValidation();
+*/
+}());

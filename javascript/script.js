fd1ebd4eeffed15b04d1dfc1eb5449fdbbe6caad
NycollSM <nycollsotomorales21@gmail.com>
2019-10-27 20:06:17 -0600

    estilos git activity

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,12 @@
 (function (){
 //github activity
-  let container = document.getElementById('github'); 
+  let container = document.getElementById('wrapper--Act'); 
   let divInfo;
   function fetchJson() {  
     fetch('https://api.github.com/users/nycollsm/events/public')
         .then (response => response.json())
           .then(data => {
-            let items = data.slice(0, 3);
+            let items = data.slice(0, 4);
             for (const repositories of items){
               divInfo = document.createElement('div');
               divInfo.setAttribute('class', 'git--items');
@@ -39,23 +39,6 @@
     
 } fetchJson();
 
-  function commitResponse(){
-    fetch('https://api.github.com/users/nycollsm/events/public')
-    .then(commit_response => commit_response.json())
-    .then(commit_data => {
-      let reposCommits = commit_data.slice(0,12);
-      //console.log(commits);
-      for(const commits_info of reposCommits) {
-          const newCommit = commits_info.payload.commits;
-          for (const commitInfo in newCommit){
-            for (messageCommit in commitInfo){
-              console.log(messageCommit.message);
-            }
-          }
-      }
-    })
-
-  }commitResponse();
 
   function hearderAnimation (){
     const headerContainer = document.getElementById('headername');
